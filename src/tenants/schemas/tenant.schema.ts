106aa58b15eb57ed2b282739745bca1f40f9b48a
Prince Narteh <prinarteh@gmail.com>
2024-06-21 08:45:46 +0000

    feat: minor changes

--- src/tenants/schemas/tenant.schema.ts
+++ src/tenants/schemas/tenant.schema.ts
@@ -1,9 +1,15 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type TenantDocument = HydratedDocument<Tenant>;
 
 @Schema()
-export class Tenant {}
+export class Tenant {
+  @Prop({ required: true })
+  companyName: string;
+
+  @Prop({ required: true, unique: true })
+  tenantId: string;
+}
 
 export const TenantSchema = SchemaFactory.createForClass(Tenant);
--- src/tenants/tenants.service.ts
+++ src/tenants/tenants.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Tenant, TenantDocument } from './schemas/tenant.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TenantsService {}
+export class TenantsService {
+  constructor(
+    @InjectModel(Tenant.name)
+    private readonly tenantModel: Model<Tenant>,
+  ) {}
+
+  async getTenantById(id: string): Promise<TenantDocument> {
+    return this.tenantModel.findById(id);
+  }
+}
