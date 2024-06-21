5734f08130038d7e4910aef29fe412e61e83d6bc
Prince Narteh <prinarteh@gmail.com>
2024-06-21 13:55:52 +0000

    feat: add create and get tenants

--- src/tenants/tenants.controller.ts
+++ src/tenants/tenants.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TenantsService } from './tenants.service';
+import { CreateTenantDto } from './dtos/create-tenant.dto';
+import { TenantDocument } from './schemas/tenant.schema';
 
-@Controller()
-export class TenantsController {}
+@Controller('tenants')
+export class TenantsController {
+  constructor(private readonly tenantsService: TenantsService) {}
+
+  @Get()
+  async getAllTenants(): Promise<TenantDocument[]> {
+    return this.tenantsService.getAllTenants();
+  }
+
+  @Get(':id')
+  async getTenantById(@Param('id') id: string): Promise<TenantDocument> {
+    return this.tenantsService.getTenantById(id);
+  }
+
+  @Post()
+  async createTenant(
+    @Body() createTenantDto: CreateTenantDto,
+  ): Promise<TenantDocument> {
+    return this.tenantsService.create(createTenantDto);
+  }
+}
--- src/tenants/tenants.service.ts
+++ src/tenants/tenants.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Tenant, TenantDocument } from './schemas/tenant.schema';
 import { Model } from 'mongoose';
+import { v4 as uuid } from 'uuid';
+import { CreateTenantDto } from './dtos/create-tenant.dto';
 
 @Injectable()
 export class TenantsService {
@@ -10,7 +12,18 @@ export class TenantsService {
     private readonly tenantModel: Model<Tenant>,
   ) {}
 
+  async getAllTenants(): Promise<TenantDocument[]> {
+    return this.tenantModel.find();
+  }
+
   async getTenantById(id: string): Promise<TenantDocument> {
     return this.tenantModel.findById(id);
+  }
+
+  async create(createTenantDto: CreateTenantDto): Promise<TenantDocument> {
+    return this.tenantModel.create({
+      ...createTenantDto,
+      tenantId: `tenant-${uuid()}`,
+    });
   }
 }
