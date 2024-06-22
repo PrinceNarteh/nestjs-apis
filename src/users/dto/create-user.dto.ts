f6277aa0ee2f03ec2494f767e54c846ebfa0b4c1
Prince Narteh <prinarteh@gmail.com>
2024-06-22 22:59:35 +0000

    feat: minor changes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Min(6, { message: 'Password must have at least six(6) characters' })
+  password: string;
+}
